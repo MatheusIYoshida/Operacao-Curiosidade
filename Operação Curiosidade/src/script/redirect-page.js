5ced0962aac51602d8956dbcf1f67f3edf639b92
Matheus <matheus.yoshida@forlogic.net>
2025-06-05 14:32:02 -0300

    #96111 adding details

--- Operação Curiosidade/src/script/redirect-page.js
+++ Operação Curiosidade/src/script/redirect-page.js
@@ -2,6 +2,17 @@ function loginChangePage(){
     var emailInput = document.getElementById("email-input").value;
     var passwordInput = document.getElementById("password-input").value;
 
+    if(emailInput == 0){
+        var email = document.getElementById("email-input");
+        email.style.border = "2px solid red";
+        email.nextElementSibling.style.display = "block";
+    }
+    if(passwordInput == 0){
+        var password = document.getElementById("password-input");
+        password.style.border = "2px solid red";
+        password.nextElementSibling.style.display = "block";
+    }
+
     if(emailInput != 0 && passwordInput != 0){
         window.location.href = "dashboard-page.html";
     }
