9323fee750737b864ef0b74569cfea1d7df1c649
Matheus <matheus.yoshida@forlogic.net>
2025-07-21 14:54:02 -0300

    #100333 storing logs in a json file

--- Operação Curiosidade/api/Server/Repositories/LogRepository.cs
+++ Operação Curiosidade/api/Server/Repositories/LogRepository.cs
@@ -1,4 +1,5 @@
 ﻿using Server.Models;
+using Server.Services;
 using static System.Runtime.InteropServices.JavaScript.JSType;
 
 namespace Server.Repositories;
@@ -7,6 +8,16 @@ public class LogRepository : ILogRepository
 {
     private static List<Log> _logs = new List<Log>();
     private static int _countId = 1;
+    private const string FilePath = "Data/logs.json";
+    private readonly IDataService _logData;
+
+    public LogRepository(IDataService logData)
+    {
+        _logData = logData;
+        _logs = _logData.LoadData<Log>(FilePath) ?? new List<Log>();
+        _countId = _logs.Count > 0 ? _logs.Max(l => l.Id) + 1 : 1;
+    }
+
     public IEnumerable<Log>? GetLogs()
     {
         return _logs.ToList();
@@ -25,6 +36,7 @@ public class LogRepository : ILogRepository
         log.Id = _countId++;
         log.CreatedAt = DateTime.UtcNow;
         _logs.Add(log);
+        _logData.SaveData(FilePath, _logs);
         return log;
     }
 
