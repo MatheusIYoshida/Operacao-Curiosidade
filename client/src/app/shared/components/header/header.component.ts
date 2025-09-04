20fc88fb6cffd71e645f7199360ddcd7e1cfbd3a
Matheus <yoshidaimatheus@gmail.com>
2025-09-04 16:38:21 -0300

    feat (header): create header component

--- client/src/app/shared/components/header/header.component.ts
+++ client/src/app/shared/components/header/header.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-header',
@@ -6,5 +7,12 @@ import { Component } from '@angular/core';
   styleUrl: './header.component.scss'
 })
 export class HeaderComponent {
+  profileIcon: string = 'N';
+  profileName: string = 'Name';
 
+  constructor(private _router: Router){}
+
+  logout(){
+    this._router.navigate(['/auth/login']);
+  }
 }
